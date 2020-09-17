09c288d346068e40ae93936bb919ac1d8a3f608e
Keels <1400785259121004@mil>
2020-09-17 16:15:03 -0600

    Added comments.

--- updateNotifyScript.ps1
+++ updateNotifyScript.ps1
@@ -7,13 +7,14 @@ $body = @("ALCON,`n`nThere is an update available! The server will be down for a
 "ALCON,`n`nThe update to $version is complete! Please clear your cache and close your browser for all changes to apply. If there are any issues or concerns, contact IT Support for assistance.$Signature")
 $to = <<toEmail>>
 
-
+#Sends Email to end users for Notification of the update
 function startEmail() {
     Send-MailMessage -From <<Email>> -To $to -Subject $subject[0] `
     -Body $body[0] `
     -SmtpServer <<SMTP RELAY>> -Port 587 -Credential $creds -UseSsl
 }
 
+#Check for any potential Database updates with the coming version. Calls runUpdate.
 function dbUpdate() {
     .\callsql.bat
     pause
@@ -26,9 +27,10 @@ function dbUpdate() {
     }
 }
 
+#Interacts with GIT-Bash terminal to run git Pull 
 function runUpdate() {
     start-process { C:\Program Files\Git\git-bash.exe }
-    sleep 30
+    sleep 30 #waits for terminal to be operational
     $wshell = New-Object -ComObject wscript.shell;
     $wshell.AppActivate('MINGW64:/p/')    
     $wshell.SendKeys('cd /d/app')
@@ -43,7 +45,8 @@ function runUpdate() {
     Start-Sleep 3
     finishEmail
 }
- 
+
+#Sends Email to end users for completion of the update
 function finishEmail() {
     Send-MailMessage -From <<email>> -To $to -Subject $subject[1] `
     -Body $body[1] `
